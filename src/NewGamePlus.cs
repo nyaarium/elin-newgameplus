d88583e198fdd8a7427b2014f9c1f485b3d0b374
Nyaarium <project@nyaarium.com>
2026-02-13 01:14:18 -0800

    Missed this verison. Now I got 4 places to keep track of....

--- src/NewGamePlus.cs
+++ src/NewGamePlus.cs
@@ -8,7 +8,7 @@ using HarmonyLib;
 namespace NewGamePlus;
 
 [BepInDependency("evilmask.elinplugins.modoptions", BepInEx.BepInDependency.DependencyFlags.SoftDependency)]
-[BepInPlugin("nyaarium.newgameplusplus", "New Game++", "1.0.0")]
+[BepInPlugin("nyaarium.newgameplusplus", "New Game++", "1.2.1")]
 public class NewGamePlus : BaseUnityPlugin
 {
 	private static NewGamePlus instance;
