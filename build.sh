fe92745e087d1253a7ec2ca886aa49be66c49dba
Nyaarium <project@nyaarium.com>
2026-02-22 02:33:54 -0800

    New Devcontainer

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 echo Building...
 
-ELIN_ROOT="/mnt/s/Steam/steamapps/common/Elin"
+ELIN_ROOT="/mnt/elin"
 
 dotnet build -c Release -p:ELIN_INSTALL_PATH="$ELIN_ROOT" -p:DeployPath="$ELIN_ROOT" 2>&1
 
