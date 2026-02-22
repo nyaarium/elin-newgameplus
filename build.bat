173c046784e543306b5864af54f0c854b58906e3
Nyaarium <project@nyaarium.com>
2026-02-22 00:46:48 -0800

    Linux build script

--- build.bat
+++ build.bat
@@ -3,8 +3,9 @@ setlocal
 
 echo Building...
 
-REM Build the project
-dotnet build -c Release 2>&1
+set DEPLOY_PATH=S:\Steam\steamapps\common\Elin
+
+dotnet build -c Release -p:DeployPath=%DEPLOY_PATH% 2>&1
 
 if %ERRORLEVEL% NEQ 0 (
     echo.
---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo Building...
+
+ELIN_ROOT="/mnt/s/Steam/steamapps/common/Elin"
+
+dotnet build -c Release -p:ELIN_INSTALL_PATH="$ELIN_ROOT" -p:DeployPath="$ELIN_ROOT" 2>&1
+
+echo
+echo Build successful!
