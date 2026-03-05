d59eb4113a3de970fe880107be6f8241f39072fa
Nyaarium <project@nyaarium.com>
2026-03-04 23:57:39 -0800

    Preserve new UID during restoration of _ints array to prevent registry corruption

--- src/Utils/ThingUtils.cs
+++ src/Utils/ThingUtils.cs
@@ -116,10 +116,18 @@ public static class ThingUtils
 		}
 
 		// Restore full _ints array (mirrors game deserialization - _ints is the source of truth)
+		// Preserve the new UID assigned by ThingGen.Create to avoid registry corruption
 		if (thingData.ints != null && thingData.ints.Length > 0)
 		{
+			int newUid = (card._ints != null && card._ints.Length > CardIntsIndices.UidOrType)
+				? card._ints[CardIntsIndices.UidOrType]
+				: 0;
 			card._ints = new int[thingData.ints.Length];
 			Array.Copy(thingData.ints, card._ints, thingData.ints.Length);
+			if (newUid != 0 && card._ints.Length > CardIntsIndices.UidOrType)
+			{
+				card._ints[CardIntsIndices.UidOrType] = newUid;
+			}
 		}
 
 		// Unpack _bits1/_bits2 from _ints (mirrors Card._OnDeserialized - game never calls ChangeMaterial on load)
